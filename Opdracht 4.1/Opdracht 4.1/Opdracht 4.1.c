5631f77d3380b464ad3bdf79f58cc84319ba2e1e
robinvanw <robinvanw@live.nl>
2015-01-05 18:21:25 +0100

    Update Opdracht 4.1.c

--- Opdracht 4.1/Opdracht 4.1/Opdracht 4.1.c
+++ Opdracht 4.1/Opdracht 4.1/Opdracht 4.1.c
@@ -3,34 +3,31 @@
 
 int main(void)
 {
-	long catalan(int n);
-	int getal, uitkomst;
+	long long catalan  (int n);
+	long long faculteit(long long n);
+	int getal;
 
 	printf("\n\nWelkom\n");
 
 	printf("\nVoer een geheel getal in : ");
 	scanf("%d", &getal);
 
-	uitkomst = catalan(getal);
+	catalan(getal);
 
+	getchar();
+	getchar();
+}
+
+long long catalan(int n)
+{
+	long long uitkomst;
+	uitkomst = (faculteit(2 * n)) / (faculteit(n)*faculteit(n + 1));
 	printf("\nUw berekende waarde is   : %d", uitkomst);
-	getchar();
-	getchar();
 }
 
-long catalan(int n)
+long long faculteit(long long n)
 {
-	long uitkomst;
-
-	uitkomst = (faculteit(2 * n)) / (faculteit(n*faculteit(n + 1)));
-
-	return uitkomst;
-
-}
-
-int faculteit(int n)
-{
-	int i, res = 1;
-	for (i = 1; i <= n; i = i + 1) { res = res * i; }
+	long long i, res = 1;
+	for (i = 1; i <= n; i = i + 1) {res = res * i;}
 	return res;
 }
